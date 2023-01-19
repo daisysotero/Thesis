663b73cc94b405adbc1ad2805f2d919e8f319d38
Daisy Sotero Chacon <84479478+daisysotero@users.noreply.github.com>
2023-01-18 21:49:16 -0300

    Delete transdecoder_Evelu.sh

--- Chapter_1/Busco/transdecoder_Evelu.sh
+++
@@ -1,7 +0,0 @@
-conda creat --name predict
-conda activate predict
-conda install -c bioconda transdecoder
-mkdir Evelu_transdecoder
-TransDecoder.LongOrfs -t Transcript_17KEvelu.fasta &
-TransDecoder.Predict -t Transcript_17KEvelu.fasta
-
